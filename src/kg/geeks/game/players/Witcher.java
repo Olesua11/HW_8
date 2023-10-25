0034365aa56b0e75fadf951247ac7cac495d2c18
Olesua11 <146833507+Olesua11@users.noreply.github.com>
2023-10-26 00:49:41 +0600

    Update Witcher.java

--- src/kg/geeks/game/players/Witcher.java
+++ src/kg/geeks/game/players/Witcher.java
@@ -5,24 +5,15 @@ public class Witcher extends Hero {
         super(health, damage, name, SuperAbility.RESURRECTION);
     }
 
-    public void resurrection(Hero[] heroes) {
-        for (int i = 0; i < heroes.length; i++) {
-            if (heroes[i].getHealth() <= 0) {
-                heroes[i].setHealth(1);
-                System.out.println("Witcher " + this.getName() + " uses Resurrection to bring a fallen hero back to life.");
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        for (Hero hero : heroes) {
+            if (hero.getHealth() <= 0) {
+                hero.setHealth(50);
+                System.out.println("Witcher " + this.getName() + " uses Resurrection to bring a fallen hero back to life: " + hero.getName());
                 this.setHealth(0);
                 break;
             }
         }
-    }
-
-    @Override
-    public void attack(Boss boss) {
-        System.out.println("Witcher " + this.getName() + " does not attack the boss.");
-    }
-
-    @Override
-    public void applySuperPower(Boss boss, Hero[] heroes) {
-
     }
 }
